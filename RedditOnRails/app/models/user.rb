f2ce1da4c40588c539beeace2353a1af20260600
adam <ayl292@nyu.edu>
2019-01-01 15:13:48 -0500

    Implement Auth Login/Logout

---
+++ RedditOnRails/app/models/user.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string
+#  session_token   :string           not null
+#
+
+class User < ApplicationRecord
+    validates_presence_of :username, :session_token
+    validates :password_digest, presence: { message: 'Password cannot be blank' }
+    validates :username, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+
+    #FGRIPE
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil 
+        end
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save! 
+        self.session_token
+    end
+
+    def is_password?(value)
+        bcrypted_pw = BCrypt::Password.new(self.password_digest)
+        bcrypted_pw.is_password?(value)
+    end
+
+    def password=(value)
+        @password = value
+        self.password_digest = BCrypt::Password.create(@password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+end
